34b6b09ae569293b9fe56e7ec1d80c2e4107a441
samuelfinatto <samueldfinatto@gmail.com>
2020-10-25 18:03:54 -0300

    include identifier function

--- identifier/src/identifier.c
+++ identifier/src/identifier.c
@@ -1,5 +1,26 @@
 #include "identifier.h"
 
-int identifier(char *ident){
-  return 0;
+int identifier(char * ident)
+{
+  char achar;
+  int  length, valid_id;
+  length = 0;
+  valid_id = valid_s(ident[0]);
+  if(valid_id) {
+    length = 1;
+  }
+  achar = ident[1];
+  while(achar != '\n') {
+    if(!(valid_f(achar))) {
+      valid_id = 0;
+    }
+    length++;
+    achar = ident[length];
+  }
+  if (valid_id && (length >= 1) && (length < 6)) {
+    return 0;
+  }
+  else {
+    return 1;
+  }
 }
