c68885077bc62c094e32ba42901a70d96dec3e48
Luís Becker <luis.becker@acad.pucrs.br>
2020-11-07 11:53:33 -0300

    teste travis

--- commiter.sh
+++ commiter.sh
@@ -3,4 +3,4 @@ git config --global github.token $GITHUB_TOKEN
 git add coverage/*
 git pull
 git commit -m "travis test files"
-git push https://$(GITHUB_TOKEN)@github.com/LufeSantos/TCS.git
+git push https://$(GITHUB_TOKEN)@github.com/LufeSantos/TCS.git HEAD:warmup --quiet
